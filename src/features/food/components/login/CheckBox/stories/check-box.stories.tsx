7da7a041cb9910e5eb427867f98246657a4fb128
unknown <33asasergey33@gmail.com>
2022-05-19 22:26:57 +0300

    fix: checkbox story

--- src/features/food/components/login/CheckBox/stories/check-box.stories.tsx
+++ src/features/food/components/login/CheckBox/stories/check-box.stories.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { CheckBox } from '../check-box';
+import { CheckBox, CheckboxProps } from '../check-box';
 
 export default {
   title: 'features/food/components/CheckBox',
   component: CheckBox,
 } as Meta;
 
-const Template: Story = (args) => <CheckBox {...args} />;
+const Template: Story<CheckboxProps> = (args) => <CheckBox {...args} />;
 
 export const Basic = Template.bind({});
 
 Basic.args = {
-  children: 'CheckBox',
+  announce: 'checkbox',
 };
